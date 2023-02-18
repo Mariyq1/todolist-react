1d06eb24e7636e543e141dc2e56bd8f518241353
Mariyq1 <m4697597@gmail.com>
2023-02-18 19:12:22 +0100

    Create useState for click buttons

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FilterValuesType } from "./App";
 export type TaskType ={
     id: number
     title: string
@@ -7,10 +8,12 @@ export type TaskType ={
 type PropsType = {
     title: string
     tasks: Array<TaskType>
+    removeTask: (id:number) => void
+    changeFilter: (value: FilterValuesType) => void
+    
 }
 
 export function Todolist(props: PropsType){
-  
   return (
     <div>
       <h3>{props.title}</h3>
@@ -19,15 +22,22 @@ export function Todolist(props: PropsType){
         <button>+</button>
       </div>
       <ul>
-        <li><input type="checkbox" checked={props.tasks[0].isDone}/><span>{props.tasks[0].title}</span></li>
-        <li><input type="checkbox" checked={props.tasks[1].isDone}/><span>{props.tasks[1].title}</span></li>
-        <li><input type="checkbox" checked={props.tasks[2].isDone}/><span>{props.tasks[2].title}</span></li>
+        {
+          props.tasks.map((t)=>{
+            return <li key={t.id}><input type="checkbox" checked={t.isDone}/>
+            <span>{t.title}</span>
+            <button onClick={()=> {props.removeTask(t.id)}}>X</button>
+            </li>
+          }
 
+          )
+        }
+        
       </ul>
       <div>
-        <button>All</button>
-        <button>Active</button>
-        <button>Complited</button>
+        <button onClick={()=> {props.changeFilter("all")}}>All</button>
+        <button onClick={()=> {props.changeFilter("active")}}>Active</button>
+        <button onClick={()=> {props.changeFilter("completed")}}>Complited</button>
       </div>
     </div>
 
